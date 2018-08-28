46d1cc80679ea81a486379fdb5d2656ce83b544b
Yanique Duke <yduke926@gmail.com>
2018-08-28 10:09:58 -0400

    Sdashboard

---
+++ client/src/models/student.js
@@ -0,0 +1,5 @@
+let express = require('express');
+let mongoose = require('mongoose');
+let router = express.Router();
+let Studentdash = mongoose.model('Studentdash');
+
---
+++ client/src/screens/Studentdash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+
+render ();
+{
+    return (
+        <div>
+            <h1>Student Name</h1>
+        </div>
+    )
+};
