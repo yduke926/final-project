922783ff6d4b2f623f754a5c19b4b7e78e60b3a1
Yanique Duke <yduke926@gmail.com>
2018-08-31 14:52:20 -0400

    Added student and admin dashboards and routes

--- client/src/models/student.js
+++ client/src/models/student.js
@@ -1,5 +1,7 @@
-let express = require('express');
+/*let express = require('express');
 let mongoose = require('mongoose');
 let router = express.Router();
 let Studentdash = mongoose.model('Studentdash');
+
+*/
 
